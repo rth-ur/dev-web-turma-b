ea57f278a314bf2edfb2213669e2285d438885cd
Arthur Gabriel Moreira Clemente <arthur.clemente@dinfor.uniceub>
2026-03-25 21:40:17 -0300

    Aula25/03 VUE

---
+++ ArthurClemente/Aulas/vue_cli/src/components/HomeComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <footer>
+            <p>Bem Vindo! De novo{{ nome }}Olá mundo do <b>Vue com CLI</b> </p>
+        </footer>
+    </div>
+    </template>
+
+<script>
+import { normalizeClass } from 'vue';
+
+    export default{
+        name: "HomeComponent",
+        data(){
+            return{
+                nome: "Arthur"
+            };
+        }
+    }
+ </script>
